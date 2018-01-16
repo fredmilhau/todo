df8f574f02302955828baed9a57dd218096b1caa
fredmilhau <fred.milhau@gmail.com>
2018-01-16 19:08:32 +0100

    add components

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {TaskModel} from '../common/models/task.model';
+import {TaskService} from '../common/task.service';
 
 @Component({
   selector: 'ta-task',
@@ -8,13 +9,21 @@ import {TaskModel} from '../common/models/task.model';
 })
 export class TaskComponent implements OnInit {
 
+  checked: boolean;
+
   @Input()
   taskModel: TaskModel;
 
-  constructor() {
+  constructor(private taskService: TaskService) {
   }
 
   ngOnInit() {
+    this.checked = this.taskModel.completed;
+  }
+
+  changeState() {
+    this.taskModel.completed = this.checked;
+    this.taskService.updateTask(this.taskModel);
   }
 
 }

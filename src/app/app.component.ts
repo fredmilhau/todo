865b3e3a5a92ba1a2d975301bd2fb3ed298ed0e7
fredmilhau <fred.milhau@gmail.com>
2018-01-16 21:43:53 +0100

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TaskService} from './common/task.service';
 
 @Component({
   selector: 'ta-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ta';
+export class AppComponent implements OnInit {
+
+  constructor(private taskService: TaskService) {
+  }
+
+  ngOnInit() {
+    // load server's data into the store
+    this.taskService.loadTasks();
+  }
+
 }
